52435238fb689cc5df238c9e118b101ae223b5ad
doheeing <doheeehan@naver.com>
2024-03-21 23:52:19 +0900

    class component 수정

--- src/AppClass.js
+++ src/AppClass.js
@@ -1,7 +1,6 @@
+import React, { Component } from "react";
 import "./App.css";
-import Box from "./component/Box";
-
-import React, { Component } from 'react'
+import BoxClass from "./component/BoxClass";
 
 const choice = {
   rock: {
@@ -16,77 +15,79 @@ const choice = {
     name: "paper",
     img: "https://www.pngitem.com/pimgs/m/592-5920636_rock-paper-scissors-clipart-rock-paper-scissors-png.png",
   },
-}
+};
 
 export default class AppClass extends Component {
-
-  constructor(props){
-    super(props);
-    this.state = ({
-      userSelect : null,
-      ComputorSelect : null, 
-      result : "",
-      comResult : "",
-      resultBox : ""
-    })
+  constructor() {
+    super();
+    this.state = {
+      userSelect: null,
+      computerSelect: null,
+      result: "",
+      comResult: "",
+    };
   }
 
-    play = (userChoice) =>{
-      let computorChoice = this.randomChoice();
-      this.setState({
-        userSelect: choice[userChoice],
-        ComputorSelect: computorChoice,
-        result : this.judgement(choice[userChoice], computorChoice),
-        comResult : this.reverseJudgement(choice[userChoice], computorChoice)
-      })
-    }
-    judgement = (user, computor) => {
-    if (user.name === computor.name) {
+  play = (userChoice) => {
+    let computerChoice = this.randomChoice();
+    this.setState({
+      userSelect: choice[userChoice],
+      computerSelect: computerChoice,
+      result: this.judgement(choice[userChoice], computerChoice),
+      comResult: this.reverseJudgement(choice[userChoice], computerChoice),
+    });
+  };
+  judgement = (user, computer) => {
+    if (user.name === computer.name) {
       return "tie";
     } else if (user.name === "rock")
-      return computor.name === "scissor" ? "win" : "lose";
+      return computer.name === "scissor" ? "win" : "lose";
     else if (user.name === "paper")
-      return computor.name === "rock" ? "win" : "lose";
+      return computer.name === "rock" ? "win" : "lose";
     else if (user.name === "scissor")
-      return computor.name === "paper" ? "win" : "lose";
-    };
-    reverseJudgement = (user, computor) => {
-      if (user.name === computor.name) {
-        return "tie";
-      } else if (user.name === "rock")
-        return computor.name === "scissor" ? "lose" : "win";
-      else if (user.name === "paper")
-        return computor.name === "rock" ? "lose" : "win";
-      else if (user.name === "scissor")
-        return computor.name === "paper" ? "lose" : "win";
-    };
-    randomChoice = () => {
+      return computer.name === "paper" ? "win" : "lose";
+  };
+  reverseJudgement = (user, computer) => {
+    if (user.name === computer.name) {
+      return "tie";
+    } else if (user.name === "rock")
+      return computer.name === "scissor" ? "lose" : "win";
+    else if (user.name === "paper")
+      return computer.name === "rock" ? "lose" : "win";
+    else if (user.name === "scissor")
+      return computer.name === "paper" ? "lose" : "win";
+  };
+  randomChoice = () => {
     let itemArray = Object.keys(choice);
     let randomItem = Math.floor(Math.random() * itemArray.length);
     let final = itemArray[randomItem];
     return choice[final];
-    }
-
-
-    render() {
-      return (
+  };
+  render() {
+    return (
       <div className="containor">
         <div className="mainbox">
           <div className="main">
-            <Box title="You" item={userSelect} result={result} />
-            <Box title="Computor" item={ComputorSelect} result={comResult} />
+            <BoxClass 
+            title="You" 
+            item={this.state.userSelect} 
+            result={this.state.result} />
+            <BoxClass
+              title="Computer"
+              item={this.state.computerSelect}
+              result={this.state.comResult}
+            />
           </div>
           <div className="button">
-            <button className="buttoneach" onClick={() => play("scissor")}>가위</button>
-            <button className="buttoneach"onClick={() => play("rock")}>바위</button>
-            <button className="buttoneach" onClick={() => play("paper")}>보</button>
+            <button className="buttoneach" onClick={() => this.play("scissor")}>가위</button>
+            <button className="buttoneach" onClick={() => this.play("rock")}>바위</button>
+            <button className="buttoneach" onClick={() => this.play("paper")}>보</button>
           </div>
         </div>
       </div>
-      )
+    );
   }
 }
-
 
 // const choice = {
 //   rock: {
@@ -104,7 +105,7 @@ export default class AppClass extends Component {
 // };
 // function App() {
 //   const [userSelect, setUserSelect] = useState(null);
-//   const [ComputorSelect, setComputorSelect] = useState(null);
+//   const [ComputerSelect, setComputorSelect] = useState(null);
 //   const [result, setResult] = useState("");
 //   const [comResult, setComResult] = useState("");
 //   const play = (userChoice) => {
